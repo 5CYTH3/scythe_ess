6e92210874aa112f4d2f6189707ac4b1a6f6936e
5CYTH3 <ScytheD@outlook.fr>
2021-07-20 18:13:30 +0200

    🥫Migrated the project to Cobra and promptui

---
+++ cmd/create.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+)
+
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "This CLI is used to generate boilerplate code for a lot of projects easily.",
+	Long:  `A CLI only made by Scythe and for Scythe to tidy up my work (Of course I am Scythe ! Contact me at Scythe@outlook.fr)`,
+	Run: func(cmd *cobra.Command, args []string) {
+		color.Blue("\nUsage : command <commands> [@argument]")
+		fmt.Println("Here are the types of projects you can create :")
+		fmt.Println(" - Web Boilerplate (web-boilerplate)\n - Golang project (go)\n")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(createCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// createCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module scythe.fr/cli-scythe
 
 go 1.16
 
-require github.com/fatih/color v1.12.0 // indirect
+require (
+	github.com/fatih/color v1.12.0
+	github.com/juju/ansiterm v0.0.0-20210706145210-9283cdf370b5 // indirect
+	github.com/lunixbochs/vtclean v1.0.0 // indirect
+	github.com/manifoldco/promptui v0.8.0 // indirect
+	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/spf13/cobra v1.2.1 // indirect
+	github.com/spf13/viper v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+)
